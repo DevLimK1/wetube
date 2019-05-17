63ac10e90da24f73a37ff7019dbf74085616510a
DevLimK1 <49403147+DevLimK1@users.noreply.github.com>
2019-05-17 19:49:23 +0900

    #2.9 MVC Pattern part One

--- app.js
+++ app.js
@@ -4,26 +4,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+
 const app = express();
-
-// function handleHome(req, res) {
-//   console.log(req);
-//   res.send("Hello frome home");
-// }
-
-const handleHome = (req, res) => res.send("Hello from home");
-
-// function handleProfile(req, res) {
-//   res.send("You ar on my profile");
-// } 아래 코드랑 똑같음
-
-const handleProfile = (req, res) => res.send("You are on my profile");
-
-// const betweenHome = (req, res, next) => {  미들웨어관련 (middleware)
-//   console.log("Between");
-//   next();
-// };
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -32,11 +17,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter);
+app.use("/video", videoRouter);
+
 // 누군가 내 파일을 불러올때(import)
 // app object를 주겠다는 의미
 export default app;
---
+++ routers/globalRouter.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const globalRouter = express.Router();
+
+export default globalRouter;
+
+// M :data
+// V :how does the data look
+// C : function that looks for the data
